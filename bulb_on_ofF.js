c3ec99fa887652e0fcc2735c72247119afdaf0d4
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2022-12-18 12:16:35 +0530

    Add files via upload

---
+++ bulb_on_ofF.js
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<h2>What Can JavaScript Do?</h2>
+
+<p>JavaScript can change HTML attribute values.</p>
+
+<button onclick="document.getElementById('myImage').src='pic_bulbon.gif'">Turn on the light</button>
+
+<img id="myImage" src="pic_bulboff.gif" ></img>
+<button onclick="document.getElementById('myImage').src='pic_bulboff.gif'">Turn of bulb</button>
+</body>
+</html>
